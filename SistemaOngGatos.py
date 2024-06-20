6eb7cd9d58f746d416b704a9ccb4d5c24e85133f
XavierDavidm <davidmoreirax15@hotmail.com>
2024-06-19 21:09:52 -0300

    Create SistemaOngGatos.py

---
+++ SistemaOngGatos.py
@@ -0,0 +1,44 @@
+#funções 
+
+#1 Cadastrar felino
+def CadastrarFelino():
+    pass
+
+#2 Alterar status do felino
+def AlterarStatus():
+    pass
+
+#3 Consultar informações sobre o felino
+def ConsultarInfo():
+    pass
+
+#4 Apresentar estatísticas gerais
+def Estatisticas():
+    pass
+
+#5 Filtragem de dados
+
+
+#6 Salvar
+
+
+#7 Sair do programa
+
+
+
+
+#main
+
+
+
+
+
+
+
+
+
+
+
+
+
+
